5a62b2bf3c6637a91259f5693f9a52ae9edd0a5c
lingzhuzi <qdyuyh@126.com>
2014-05-01 10:18:08 +0800

    update: add android demos downloader and update the code of demos

---
+++ android_demos_downloader.js
@@ -0,0 +1,172 @@
+var fs = require('fs'),
+    $ = require('jquery');
+
+var fileList = {},
+    images = {},
+    jsonFileName = 'file_list_json';
+
+
+function getFileLists() {
+    fs.exists(jsonFileName, function (exists) {
+        if (exists) {
+            fs.readFile(jsonFileName, 'utf8', function (err, data) {
+                if (err) {
+                    throw err;
+                }
+                fileList = JSON.parse(data);
+            });
+        } else {
+            parseIndexPage();
+        }
+    })
+}
+
+function parseIndexPage() {
+    $.get('http://developer.android.com/samples/index.html', function (doc) {
+        $(doc).find('#nav').children('.nav-section').each(function (_, li) {
+            parseFileLists($(li), fileList);
+        });
+        //写入文件
+        var text = JSON.stringify(fileList);
+        fs.writeFile(jsonFileName, txt, function (err) {
+            if (err) throw err;
+            console.log('It\'s saved!'); //文件被保存
+        });
+    });
+}
+
+
+function parseFileLists($section, files) {
+    var title = $section.children('.nav-section-header').children('a').attr('title');
+    if (title) {
+        files[title] = {};
+        $section.children('ul').children('li').each(function (_, li) {
+            parseFileLists($(li), files[title]);
+        });
+    } else {
+        var $a = $section.children('a');
+        if ($a.length > 0) {
+            var url = $a.attr('href');
+            var fileName = $a.attr('title');
+            files[fileName] = url;
+        }
+    }
+}
+
+function saveCodes(list, path) {
+    for (var name in list) {
+        if (list.hasOwnProperty(name)) {
+            var value = list[name];
+            if (typeof value == 'string') {
+                parseAndSave(path, name, value);
+            } else {
+                var dir_path = path + name.split('.').join('/') + '/';
+                saveCodes(value, dir_path);
+            }
+        }
+    }
+}
+
+function parseAndSave(path, name, url) {
+    fs.exists(path + name, function (exists) {
+        if (!exists) {
+            parseCodePage(path, name, url, function (file_path, file_name, code) {
+                saveCode(file_path, file_name, code);
+            });
+        }
+    });
+}
+
+function parseCodePage(path, name, url, callback) {
+    var code = '';
+    if (isImage(name)) {
+        code = 'http://developer.android.com' + (url[0] == '/' ? '' : '/') + url;
+        callback(path, name, code);
+    } else {
+        $.get("http://developer.android.com" + url, function (html) {
+            var content = $(html).find('#codesample-wrapper').text();
+            var arr = content.split("\n");
+            for (var i = 0; i < arr.length; i++) {
+                if (!/\d/.test($.trim(arr[i]))) {
+                    arr.push(arr[i]);
+                }
+            }
+            var code = arr.join('\n');
+            callback(path, name, code);
+        });
+    }
+}
+
+function saveCode(path, name, code) {
+    mkdirs(path, function () {
+        if (isImage(name)) {
+            var arr = code.split('/');
+            var imgName = arr[arr.length - 2] + '/' + arr[arr.length - 1];
+            if (images[imgName]) {
+                copy(images[imgName], path + name);
+            } else {
+                downloadImage(path + name, code);
+            }
+        } else {
+            fs.exists(path + name, function (exists) {
+                if (!exists) {
+                    console.log('saving ' + path + name);
+                    fs.writeFile(path + name, code, function (err) {
+                        if (err) throw err;
+                        //console.log('It\'s saved!'); //文件被保存
+                    });
+                }
+            })
+        }
+    });
+}
+
+function copy(from, to) {
+    console.log('copying file to ' + to);
+    var fileReadStream = fs.createReadStream(from);
+    var fileWriteStream = fs.createWriteStream(to);
+    fileReadStream.pipe(fileWriteStream);
+
+    fileWriteStream.on('close', function () {
+        console.log('copy over');
+    });
+}
+
+function downloadImage(path, url) {
+    console.log('downloading image from ' + url);
+    $.get(url, function (data) {
+        fs.writeFile(path, data, function (err) {
+            if (err) {
+                throw err
+            }
+            ;
+            console.log('download over and saved to ' + path);
+            var arr = url.split('/');
+            var imgName = arr[arr.length - 2] + '/' + arr[arr.length - 1];
+            images[imgName] = path;
+        })
+    })
+
+}
+
+function isImage(name) {
+    var arr = name.split('.');
+    return ['png', 'jpg', 'git'].indexOf(arr[arr.length - 1]) > -1;
+}
+
+// 创建所有目录
+function mkdirs(dirpath, mode, callback) {
+    fs.exists(dirpath, function (exists) {
+        if (exists) {
+            callback(dirpath);
+        } else {
+            //尝试创建父目录，然后再创建当前目录
+            mkdirs(path.dirname(dirpath), mode, function () {
+                fs.mkdir(dirpath, mode, callback);
+            });
+        }
+    });
+}
+
+getFileLists();
+saveCodes(fileList, './');
--- android_demos_downloader.rb
+++ android_demos_downloader.rb
@@ -4,6 +4,7 @@ require 'nokogiri'
 require 'open-uri'
 require 'active_support'
 require 'pathname'
+require "fileutils"
 
 class DownLoader
 
@@ -12,20 +13,32 @@ class DownLoader
     @stack = []
     @images = {}
     parse_index_page
-    current_path = Pathname.new(File.dirname(__FILE__)).realpath
+    current_path = Pathname.new(File.dirname(__FILE__)).realpath.to_s + '/'
     save_codes(@file_list, current_path)
   end
 
   def parse_index_page
-    doc = Nokogiri::HTML(open('http://developer.android.com/intl/zh-cn/samples/index.html'))
-    doc.css('#nav > .nav-section').each do |section|
-      parse_file_list(section, @file_list)
+    file_name = 'file_list_json'
+    if File.exists?(file_name)
+      puts "read file list from file #{file_name}"
+      json = ''
+      File.open(file_name) do |file|
+        file.each do |line|
+          json << line
+        end
+      end
+      @file_list = ActiveSupport::JSON.decode(json)
+    else
+      doc = Nokogiri::HTML(open('http://developer.android.com/intl/zh-cn/samples/index.html'))
+      doc.css('#nav > .nav-section').each do |section|
+        parse_file_list(section, @file_list)
+      end
+      json = ActiveSupport::JSON.encode(@file_list)
+      file = File.new(file_name, 'w')
+      file.puts json
+      file.close
+      puts "saved file list to file #{file_name}"
     end
-    # json = ActiveSupport::JSON.encode(@file_list)
-    # file = File.new('file_list_json', 'w')
-    # file.puts json
-    # file.close
-
   end
 
   def parse_file_list(section, list)
@@ -35,7 +48,9 @@ class DownLoader
       if attr
         title = attr.class.to_s == 'String' ? attr : attr.value
         list[title] = {}
-        section.children.css('li').each do |li|
+        index = 0
+        section.children.css('> li').each do |li|
+          index += 1
           parse_file_list(li, list[title])
         end
       end
@@ -52,10 +67,14 @@ class DownLoader
   def save_codes(file_list, current_path)
     file_list.each do |name, value|
       if value.class.to_s == 'String'
-        code = parse_code_page(name, value)
-        save_code(current_path, name, code)
+        unless File.exists? "#{current_path}#{name}"
+          code = parse_code_page(name, value)
+          puts "save: #{current_path}#{name}"
+          save_code(current_path, name, code)
+        end
       else
-        save_codes(file_list[name], "#{current_path}/name")
+        dir_path = current_path + "#{name.gsub(/\./, '/')}/"
+        save_codes(file_list[name], dir_path)
       end
     end
   end
@@ -66,7 +85,11 @@ class DownLoader
       code = "http://developer.android.com#{url.start_with?('/') ? '' : '/'}#{url}"
     else
       puts "parse #{url}"
-      doc = Nokogiri::HTML(open("http://developer.android.com#{url}"))
+	  t1 = Time.now
+      page = open("http://developer.android.com#{url}")
+	  t2 = Time.now
+	  puts "download takes #{t2 - t1}s"
+	  doc = Nokogiri::HTML(page)
       wrappers = doc.css('#codesample-wrapper')
       if wrappers && wrappers[0]
         arr = []
@@ -74,16 +97,49 @@ class DownLoader
           unless /\d/.match(line.strip)
             arr << line
           end
-          code = arr.join("\n")
-          puts code
         end
+        code = arr.join("\n")
+      end
+	  t3 = Time.now
+	  puts "parse page takes #{t3 - t2}s"
+	  puts "total #{t3 - t1}s"
+    end
 
+    code
+  end
+
+  def save_code(path, name, code)
+    mkdir(path)
+    if is_image?(name)
+      arr = code.split('/')
+      full_name = "#{arr[-2]}/#{arr[-1]}"
+      if @images.include?(full_name)
+        FileUtils.copy(@images[full_name], "#{path}#{name}")
+      else
+        data=open(code) { |f| f.read }
+        open("#{path}#{name}", "wb") { |f| f.write(data) }
+        @images[full_name] = "#{path}#{name}"
+      end
+    else
+      unless File.exists? "#{path}#{name}"
+        file = File.new("#{path}#{name}", 'w')
+        file.puts(code)
+        file.close
       end
     end
   end
 
-  def save_code(path, name, code)
-
+  def mkdir(path)
+    current_path = Pathname.new(File.dirname(__FILE__)).realpath.to_s
+    sub_path = path.gsub(current_path, '')
+    dir_path = ''
+    sub_path.split('/').each do |dir_name|
+      if dir_name != ''
+        dir_path << dir_name
+        Dir.mkdir dir_path unless File.exists? dir_path
+        dir_path << '/'
+      end
+    end
   end
 
 
@@ -91,7 +147,9 @@ class DownLoader
     ['png', 'jpg', 'gif'].include?(url.split('.')[-1])
   end
 
+
 end
 
 downloader = DownLoader.new
 downloader.run
+#downloader.mkdir('F:/workspace/test/Background/RepeatingAlarm/res/drawable-hdpi')
